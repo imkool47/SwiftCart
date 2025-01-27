994f2e932500e122240071d59c739f263f47d1eb
Mukul Kumar <mukul.kumar630@gmail.com>
2025-01-27 13:41:47 +0530

    refactor: enhance styling and improve product fetching logic

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,16 +1,20 @@
 import "../App.css";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useState } from "react";
 
 export const ProductList = () => {
   const [products, setProducts] = useState([]);
   const [url, setUrl] = useState("http://localhost:3000/products");
 
-  useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => setProducts(data));
+  const fetchProducts = useCallback(async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setProducts(data);
   }, [url]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
 
   return (
     <section>
